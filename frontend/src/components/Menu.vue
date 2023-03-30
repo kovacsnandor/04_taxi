ca19f280f6010df274dadff3f0248cb4a31f6242
kovacsnandor <kovacsnandor@gmail.com>
2023-03-30 12:22:55 +0200

    loginSuccess

--- frontend/src/components/Menu.vue
+++ frontend/src/components/Menu.vue
@@ -16,7 +16,9 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <router-link class="nav-link" aria-current="page" to="/">Home</router-link>
+            <router-link class="nav-link" aria-current="page" to="/"
+              >Home</router-link
+            >
           </li>
           <li class="nav-item">
             <router-link class="nav-link" to="/about">About</router-link>
@@ -32,22 +34,40 @@
               Taxiállomás
             </a>
             <ul class="dropdown-menu">
-              <li><router-link class="dropdown-item" to="/taxiFuvarjai">Taxi fuvarjai</router-link></li>
-              <li><router-link class="dropdown-item" to="/taxiKezeles">Taxi kezelés</router-link></li>
+              <li>
+                <router-link class="dropdown-item" to="/taxiFuvarjai"
+                  >Taxi fuvarjai</router-link
+                >
+              </li>
+              <li>
+                <router-link class="dropdown-item" to="/taxiKezeles"
+                  >Taxi kezelés</router-link
+                >
+              </li>
               <li><hr class="dropdown-divider" /></li>
-              <li><router-link class="dropdown-item" to="/fuvarBevitel">Fuvar bevitel</router-link></li>
+              <li>
+                <router-link class="dropdown-item" to="/fuvarBevitel"
+                  >Fuvar bevitel</router-link
+                >
+              </li>
             </ul>
           </li>
           <li class="nav-item">
-            <router-link class="nav-link disabled" to="/count1">Counter1</router-link>
+            <router-link class="nav-link disabled" to="/count1"
+              >Counter1</router-link
+            >
           </li>
           <li class="nav-item">
             <router-link class="nav-link" to="/count2">Counter2</router-link>
           </li>
-          <li class="nav-item">
+          <li class="nav-item" v-if="!storeLogin.loginSuccess">
             <router-link class="nav-link" to="/login">Login</router-link>
           </li>
-
+          <li class="nav-item" v-if="storeLogin.loginSuccess" @click="logout()">
+            <router-link class="nav-link" to="/login"
+              >logut ({{ storeLogin.userName }})</router-link
+            >
+          </li>
         </ul>
         <form class="d-flex" role="search">
           <input
@@ -61,10 +81,17 @@
       </div>
     </div>
   </nav>
-
 </template>
 
 <script setup>
+import { useUrlStore } from "@/stores/url";
+import { useLoginStore } from "@/stores/login";
+const storeUrl = useUrlStore();
+const storeLogin = useLoginStore();
+
+function logout() {
+  storeLogin.loginSuccess = 0;
+}
 </script>
 
 <style>
