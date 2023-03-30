6b1277d5007e88131601a311fa5a7557493d26d9
kovacsnandor <kovacsnandor@gmail.com>
2023-03-30 11:18:43 +0200

    login dizájn

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,7 +1,33 @@
 
 <template>
-  <div>
+  <div class="my-width-login p-5">
     <h1>Login</h1>
+    <!-- user name -->
+    <div class="mb-3">
+      <label for="userName" class="form-label"
+        >User név:</label
+      >
+      <input
+        type="text"
+        class="form-control"
+        id="userName"
+      />
+    </div>
+
+    <!-- password -->
+    <div class="mb-3">
+      <label for="pasword" class="form-label"
+        >Jelszó:</label
+      >
+      <input
+        type="text"
+        class="form-control"
+        id="pasword"
+      />
+    </div>
+
+    <button type="button" class="btn btn-primary">Login</button>
+
   </div>
 </template>
 
@@ -10,4 +36,7 @@
 
 
 <style>
+.my-width-login {
+  max-width: 500px;
+}
 </style>
